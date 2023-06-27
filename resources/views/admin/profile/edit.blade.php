10677407d2c8611f04026f33c326072ea5fe5a9e
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-06-27 03:48:20 -0400

    fix: admin profile responsiveness

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -3,12 +3,12 @@
 @section('title', 'Admin Manage Profile - FlowerGallery')
 
 @section('main-content')
-    <div class="py-20">
+    <div class="py-20 mx-auto">
         <h2 class="pb-12 font-semibold leading-7 text-gray-900 text-3xl text-center flex flex-col gap-4">
             Manage Profile
             <div class="h-1 w-24 bg-pink-500 rounded self-center"></div>
         </h2>
-        <div class="max-w-7xl mx-auto px-6 lg:px-8 space-y-6 lg:ml-[6vw]">
+        <div class="space-y-6 px-6 lg:px-12">
             <div class="p-4 bg-white shadow-md rounded-lg lg:p-0">
                 @include('admin.profile.partials.update-profile')
             </div>
