103c51d27a71dadd8b0c60f5bf5bc19128b62b47
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-06-28 13:30:04 -0400

    fix: auth guards url access

--- app/Http/Controllers/SellerAuth/AuthenticatedSessionController.php
+++ app/Http/Controllers/SellerAuth/AuthenticatedSessionController.php
@@ -8,6 +8,7 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use Illuminate\View\View;
 
 class AuthenticatedSessionController extends Controller
@@ -25,6 +26,8 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
+        Session::put('user_type', 'seller');
+
         $request->authenticate();
 
         $request->session()->regenerate();
--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -6,6 +6,7 @@ use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use Symfony\Component\HttpFoundation\Response;
 
 class RedirectIfAuthenticated
@@ -27,6 +28,13 @@ class RedirectIfAuthenticated
             if (Auth::guard($guard)->check()) {
                 return redirect(RouteServiceProvider::HOME);
             }
+        }
+
+        if(Session::get('user_type') === 'admin') {
+            return redirect(RouteServiceProvider::ADMIN_HOME);
+        }
+        elseif(Session::get('user_type') === 'seller') {
+            return redirect(RouteServiceProvider::SELLER_HOME);
         }
 
         return $next($request);
