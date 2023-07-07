e8f7bbeae7598f6cab3caffbd7e772ebcfb67c28
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-07-07 14:15:29 -0400

    add: product model and migration

---
+++ database/migrations/2014_10_14_000001_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name', 100);
+            $table->text('product_desc', );
+            $table->string('product_price');
+            $table->string('product_dimensions')->nullable();
+            $table->string('product_img1');
+            $table->string('product_img2')->nullable();
+            $table->string('product_img3')->nullable();
+            $table->string('product_img4')->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('product_categories')
+                ->onUpdate('cascade');
+            $table->unsignedBigInteger('seller_id');
+            $table->foreign('seller_id')->references('id')->on('sellers')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
