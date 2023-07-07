dfdcb184ea45766827e1679d83ba24bd647b3166
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-07-07 12:15:07 -0400

    add: manage categories routes

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -57,6 +57,14 @@
                             <span class="mx-2 text-sm md:text-md font-medium">Sellers</span>
                         </a>
 
+                        <a href="{{route('admin.categories')}}" class="{{Request::routeIs('admin.categories') ? 'bg-gray-100 text-gray-900' : 'text-gray-200'}} flex items-center px-4 py-2 mx-6 lg:mx-2 transition-colors duration-300 transform rounded-lg hover:bg-gray-100 hover:text-gray-900">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6a2.25 2.25 0 002.227 1.932H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-1.883-2.542m-16.5 0V6A2.25 2.25 0 016 3.75h3.879a1.5 1.5 0 011.06.44l2.122 2.12a1.5 1.5 0 001.06.44H18A2.25 2.25 0 0120.25 9v.776" />
+                            </svg>
+
+                            <span class="mx-2 text-sm md:text-md font-medium">Categories</span>
+                        </a>
+
                         <a class="flex items-center px-4 py-2 mx-6 lg:mx-2 text-gray-200 transition-colors duration-300 transform rounded-lg hover:bg-gray-100 hover:text-gray-900" href="#">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
--- routes/web.php
+++ routes/web.php
@@ -75,11 +75,6 @@ Route::get('/admin/dashboard', function () {
 })->middleware(['auth:admin'])->name('admin.dashboard');
 
 
-// Route::get('/admin/manage-customers', function() {
-//     return view('admin.manage_customers');
-// })->middleware(['auth:admin'])->name('admin.manage_customers');
-
-
 Route::get('/admin/manage-sellers', function() {
     return view('admin.manage_sellers');
 })->middleware(['auth:admin'])->name('admin.manage_sellers');
@@ -105,6 +100,14 @@ Route::middleware('auth:admin')->group(function () {
     Route::get('/admin/edit-customer/{id}', [ManageCustomersController::class, 'edit'])->name('admin.customer.edit');
     Route::patch('/admin/edit-customer/{id}', [ManageCustomersController::class, 'update'])->name('admin.customer.update');
     Route::get('/admin/delete-customer/{id}', [ManageCustomersController::class, 'destroy'])->name('admin.customer.destroy');
+});
+
+
+Route::middleware('auth:admin')->group(function () {
+    Route::get('/admin/manage-categories', [ManageCustomersController::class, 'index'])->name('admin.categories');
+    Route::get('/admin/edit-category/{id}', [ManageCustomersController::class, 'edit'])->name('admin.category.edit');
+    Route::patch('/admin/edit-category/{id}', [ManageCustomersController::class, 'update'])->name('admin.category.update');
+    Route::get('/admin/delete-category/{id}', [ManageCustomersController::class, 'destroy'])->name('admin.category.destroy');
 });
 
 
