708e43e1663749c8e7c6a5876234bf741d94578b
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-07-09 18:52:35 -0400

    feat: seller - update product with images

--- app/Http/Controllers/ManageProductsController.php
+++ app/Http/Controllers/ManageProductsController.php
@@ -7,6 +7,7 @@ use App\Models\Product;
 use App\Models\ProductCategory;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 
@@ -36,15 +37,15 @@ class ManageProductsController extends Controller
     {
         $request->user()->fill($request->validated());
 
-        $dirPath = '/sellers/seller_'.auth()->user()->id;
+        $dirPath = 'sellers/seller_'.auth()->user()->id;
 
         if($request->hasFile('product_images')) {
             foreach($request->product_images as $key => $value) {
                 $string = sha1(rand());
                 $randomString = substr($string, 0, 6);
                 $imgName = 'FloralGallery-seller_'.auth()->user()->id.'-'.date('Y-m-d-H-i-s-u').'-'.$randomString.'.'.$value->extension();
-                $value->move(public_path().$dirPath, $imgName);
-                $imgs[] = $imgName;
+                $value->move(public_path().'/'.$dirPath, $imgName);
+                $imgs[] = $dirPath.'/'.$imgName;
             }
         }
 
@@ -75,16 +76,78 @@ class ManageProductsController extends Controller
      */
     public function edit(Request $request): View
     {
-        $product = Product::where('id', $request->route('id'))->first();
-        return view('seller.manage-products.product.edit', ['product' => $product]);
+        // dd($request->route('id'));
+        $product = DB::table('products')
+                    ->join('product_categories', 'products.category_id',"=",'product_categories.id')
+                    ->select("products.*", 'product_categories.category')->where('products.id', $request->route('id'))->first();
+        $categories = ProductCategory::select()->get();
+        return view('seller.manage-products.product.edit', ['product' => $product, 'categories' => $categories]);
     }
 
     /**
      * Update the product information.
     */
-    public function update(Request $request): RedirectResponse
+    public function update(ProductAddOrUpdateRequest $request): RedirectResponse
     {
-        Product::where('id', $request->route('id'))->update(['product' => $request->product]);
+        
+        if(isset($request->product_images)) {
+            // Get paths of existing images.
+            $allExistingImgPaths = DB::table('products')->select('product_img1','product_img2', 'product_img3', 'product_img4')
+                                        ->where('id', $request->route('id'))->first();
+            
+            // Delete existing images one by one.
+            foreach ($allExistingImgPaths as $key => $imgPath) {
+                if(file_exists($imgPath)){
+                    unlink($imgPath);
+                }
+            }
+            
+            // Set path where updated images will be saved to.
+            $dirPath = 'sellers/seller_'.auth()->user()->id;
+
+            // Rename and Move images one by one.
+            foreach($request->product_images as $key => $value) {
+                $randomString = substr(sha1(rand().microtime()), 0, 6);
+                $imgName = 'FloralGallery-seller_'.auth()->user()->id.'-'.date('Y-m-d-H-i-s').'-'.$randomString.'.'.$value->extension();
+                $value->move(public_path().'/'.$dirPath, $imgName);
+                $imgs[] = $dirPath.'/'.$imgName;
+            }
+
+
+            if(count($request->product_images) == 1) {
+                $request['product_img1'] = $imgs[0];
+                $request['product_img2'] = NULL;
+                $request['product_img3'] = NULL;
+                $request['product_img4'] = NULL;
+            }
+
+            if(count($request->product_images) == 2) {
+                $request['product_img1'] = $imgs[0];
+                $request['product_img2'] = $imgs[1];
+                $request['product_img3'] = NULL;
+                $request['product_img4'] = NULL;
+            }
+            
+            if(count($request->product_images) == 3) {
+                $request['product_img1'] = $imgs[0];
+                $request['product_img2'] = $imgs[1];
+                $request['product_img3'] = $imgs[2];
+                $request['product_img4'] = NULL;
+            }
+            
+            if(count($request->product_images) == 4) {
+                $request['product_img1'] = $imgs[0];
+                $request['product_img2'] = $imgs[1];
+                $request['product_img3'] = $imgs[2];
+                $request['product_img4'] = $imgs[3];
+            }
+
+            Product::where('id', $request->route('id'))->update($request->except(['_token', '_method', 'product_images']));
+        }
+        else {
+            Product::where('id', $request->route('id'))->update($request->except(['_token', '_method']));
+        }
+        
         return Redirect::route('seller.products')->with('update-product-info', "Product Infromation Updated Successfully.");
     }
 
