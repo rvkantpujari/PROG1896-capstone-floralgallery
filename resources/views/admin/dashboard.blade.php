88faeec0d89397755c869c0c2c5541bbb89c9e56
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-07-20 14:01:16 -0400

    style: responsive mobile sidebar

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,7 +4,7 @@
 
 @section('main-content')
     {{-- Add Main Section Here!!! --}}
-    <div class="w-full">
+    <div class="w-full hidden md:block" id="main">
         <h1 class="text-[26px] font-semibold m-8 pt-4">Dashboard</h1>
         <div class="flex flex-col justify-center">
             @include('admin.dashboard.top_cards')
