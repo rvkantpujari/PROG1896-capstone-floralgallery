5f981b99865dd9c57b07d840a3162eea2345c0d2
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-07-22 12:18:22 -0400

    style: show message when product added or updated

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -17,6 +17,7 @@ class CartController extends Controller
 
     function add(Request $request)
     {
+        $txType = 0; // 0 - Added OR 1 - Updated
         DB::beginTransaction();
         try {
             $product = DB::table('products')->select('id', 'product_price')
@@ -27,7 +28,7 @@ class CartController extends Controller
             if($productExists) {
                 $quantity = $productExists->quantity + $request->product_quantity;
                 DB::table('cart')->where([['product_id', $request->product_id], ['user_id', auth()->user()->id]])->update(['quantity' => $quantity]);
-                return Redirect::back()->with('cart-product-updated','Product quantity updated in your cart.');
+                $txType = 1;
             } else {
                 $cart = new Cart();
                 $cart->product_id = $product->id;
@@ -39,6 +40,9 @@ class CartController extends Controller
         } catch (\Exception $e) {
             DB::rollback();
         }
+
+        if($txType)
+            return Redirect::back()->with('cart-product-updated','Product quantity updated in your cart.');
 
         return Redirect::back()->with('cart-product-added','Product added to your cart successfully.');
     }
