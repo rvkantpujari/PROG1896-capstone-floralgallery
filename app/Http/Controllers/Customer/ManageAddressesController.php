81b748bf1b328d8e84a5ad55b34a11cc1310a294
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-07-24 20:17:32 -0400

    feat: add customer delivery address

--- app/Http/Controllers/Customer/ManageAddressesController.php
+++ app/Http/Controllers/Customer/ManageAddressesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CustomerAddressRequest;
+use App\Models\UserAddresses;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
@@ -20,9 +22,22 @@ class ManageAddressesController extends Controller
         return view('customer.manage-addresses.address.add', ['provinces' => $provinces]);
     }
 
-    function add(Request $request)
+    function add(CustomerAddressRequest $request)
     {
-        
+        $request->validated();
+
+        $address = new UserAddresses();
+        $address->user_full_name = $request->full_name;
+        $address->unit = $request->unit;
+        $address->street_number = $request->street_number;
+        $address->street_name = $request->street_name;
+        $address->city = $request->city;
+        $address->postal_code = $request->postal_code;
+        $address->province_id = $request->province;
+        $address->user_id = auth()->user()->id;
+        $address->save();
+
+        return Redirect::route('customer.addresses')->with('added-user-address', "New Delivery Address Added Successfully.");
     }
 
     function update(Request $request)
