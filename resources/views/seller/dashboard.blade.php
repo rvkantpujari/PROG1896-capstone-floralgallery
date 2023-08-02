73cb70bef1bd92322aa35bf8691d0b419a605622
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-08-02 10:45:49 -0400

    style: seller dashboard verify email message

--- resources/views/seller/dashboard.blade.php
+++ resources/views/seller/dashboard.blade.php
@@ -4,28 +4,43 @@
 
 @section('main-content')
     {{-- Add Main Section Here!!! --}}
-    <h1>Hi, {{Auth::guard('seller')->user()->first_name}}. This is Seller Dashboard.</h1>
-    @if(Auth::user()->email_verified_at == null)
-        <div role="alert" class="h-auto md:h-[8vh] lg:h-[12vh] w-auto rounded border-b-4 md:border-s-4 md:border-b-0 border-red-500 bg-red-50 mx-4 px-6 py-4 text-justify absolute top-40 md:top-24 md:right-0 transition duration-300"
-            x-data="{ show: true }" x-show="show" x-transition
-            x-init="setTimeout(() => show = false, 5000)"
-        >
-            <div class="flex items-center gap-2 text-red-800">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-5 w-5">
-                    <path fill-rule="evenodd" d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd"/>
-                </svg>
-                <strong class="block font-medium"> Access Restrictions!! </strong>
-            </div>
-            
-            <p class="mt-2 text-sm text-red-700">
-                To access all features conveniently, go to 
-                <a href={{route('seller.profile.edit')}} class="text-black">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
-                    </svg>
-                    Profile
-                </a> and verify your email.
-            </p>
+    <section class="container m-8 pt-4">
+        <h1 class="text-[26px] font-semibold">Dashboard</h1>
+        <div class="pt-8">
+            @if(Auth::user()->email_verified_at == null)
+                <div class="w-full md:w-3/5 lg:w-2/5 mx-auto py-20">
+                    <article class="rounded-xl border-2 border-gray-100 bg-white shadow-lg">
+                        <div class="flex items-start gap-4 md:p-4 p-6 lg:p-8">
+                            <div>
+                                <h3 class="flex items-center gap-2 font-medium text-xl hover:underline">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                                    </svg>
+                                    Attention Required
+                                </h3>
+                            
+                                <p class="mt-2 line-clamp-2 text-sm text-gray-700">
+                                    To access all features conveniently, please <strong>verify</strong> your email.
+                                </p>
+                                
+                                <p class="mt-4 text-xs">
+                                    <a href="{{route('seller.profile.edit')}}" title="Verify Email" class="inline-block border border-pink-300 bg-pink-400 text-white rounded px-4 py-2 text-sm font-medium hover:bg-green-50 hover:border-green-300 hover:text-green-800 focus:relative">
+                                        Verify Email
+                                    </a>                 
+                                </p>
+                            </div>
+                        </div>
+                    
+                        <div class="flex justify-end">
+                            <strong class="-mb-[2px] -me-[2px] inline-flex items-center gap-1 rounded-ee-xl rounded-ss-xl bg-red-600 px-3 py-1.5 text-white">
+                                <span class="text-[14px] font-medium">Not Verified!</span>
+                            </strong>
+                        </div>
+                    </article>
+                </div>
+            @else
+                <h2>Hi, {{Auth::guard('seller')->user()->first_name}}. This is Seller Dashboard.</h2>
+            @endif
         </div>
-    @endif
+    </section>
 @endsection
--- resources/views/seller/manage_products.blade.php
+++ resources/views/seller/manage_products.blade.php
@@ -14,17 +14,50 @@
 
 @section('main-content')
     {{-- Add Main Section Here!!! --}}
-    <div class="w-full">
-        <div class="flex flex-col md:flex-row items-center md:justify-between pt-4">
-            <h1 class="text-center lg:text-left text-[26px] font-semibold m-8">Manage Products</h1>
-            @if (auth()->user()->status != 'suspended')
+    <section class="container m-8 pt-4">
+        <div class="flex flex-col md:flex-row items-center md:justify-between">
+            <h1 class="text-center lg:text-left text-[26px] font-semibold">Manage Products</h1>
+            @if (auth()->user()->status != 'suspended' && Auth::user()->email_verified_at != null)
                 <a href="{{route('seller.product.add')}}" class="px-[16px] py-[8px] md:mr-8 rounded-md text-white bg-pink-500">Add Product</a>
             @endif
         </div>
-        <div class="w-full px-8 flex justify-center">
-            @livewire('seller.products')
+        <div class="pt-8">
+            @if(Auth::user()->email_verified_at == null)
+                <div class="w-full md:w-3/5 lg:w-2/5 mx-auto py-20">
+                    <article class="rounded-xl border-2 border-gray-100 bg-white shadow-lg">
+                        <div class="flex items-start gap-4 md:p-4 p-6 lg:p-8">
+                            <div>
+                                <h3 class="flex items-center gap-2 font-medium text-xl hover:underline">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                                    </svg>
+                                    Attention Required
+                                </h3>
+                            
+                                <p class="mt-2 line-clamp-2 text-sm text-gray-700">
+                                    To access all features conveniently, please <strong>verify</strong> your email.
+                                </p>
+                                
+                                <p class="mt-4 text-xs">
+                                    <a href="{{route('seller.profile.edit')}}" title="Verify Email" class="inline-block border border-pink-300 bg-pink-400 text-white rounded px-4 py-2 text-sm font-medium hover:bg-green-50 hover:border-green-300 hover:text-green-800 focus:relative">
+                                        Verify Email
+                                    </a>                 
+                                </p>
+                            </div>
+                        </div>
+                    
+                        <div class="flex justify-end">
+                            <strong class="-mb-[2px] -me-[2px] inline-flex items-center gap-1 rounded-ee-xl rounded-ss-xl bg-red-600 px-3 py-1.5 text-white">
+                                <span class="text-[14px] font-medium">Not Verified!</span>
+                            </strong>
+                        </div>
+                    </article>
+                </div>
+            @else
+                @livewire('seller.products')
+            @endif
         </div>
-    </div>
+    </section>
 @endsection
 
 @section('js-scripts')
