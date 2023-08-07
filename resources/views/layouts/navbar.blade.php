618c50964cc8a2353cbc4c0874b627c6235b0e23
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-08-07 11:02:06 -0400

    fix: active nav link color

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -51,15 +51,15 @@
                 
                 <div class="lg:w-4/6 flex flex-row flex-wrap justify-around gap-y-2 gap-x-4 md:gap-2 my-4 md:my-0 text-black font-semibold">
 
-                    <a href="{{route('home')}}" class="px-3 py-2 my-2 lg:mt-2 transition-colors duration-300 transform rounded-md {{Request::routeIs('home') ? 'active' : ''}} hover:text-white hover:bg-gray-700">
+                    <a href="{{route('home')}}" class="px-3 py-2 my-2 lg:mt-2 transition-colors duration-300 transform rounded-md {{Request::routeIs('home') ? 'active text-pink-500' : ''}} hover:text-white hover:bg-gray-700">
                         Home
                     </a>
 
-                    <a href="{{route('about_us')}}" class="px-3 py-2 my-2 lg:mt-2 transition-colors duration-300 transform rounded-md {{Request::routeIs('about_us') ? 'active' : ''}} hover:text-white hover:bg-gray-700">
+                    <a href="{{route('about_us')}}" class="px-3 py-2 my-2 lg:mt-2 transition-colors duration-300 transform rounded-md {{Request::routeIs('about_us') ? 'active text-pink-500' : ''}} hover:text-white hover:bg-gray-700">
                         About Us
                     </a>
 
-                    <a href="{{route('contact_us')}}" class="px-3 py-2 my-2 lg:mt-2 transition-colors duration-300 transform rounded-md {{Request::routeIs('contact_us') ? 'active' : ''}} hover:text-white hover:bg-gray-700">
+                    <a href="{{route('contact_us')}}" class="px-3 py-2 my-2 lg:mt-2 transition-colors duration-300 transform rounded-md {{Request::routeIs('contact_us') ? 'active text-pink-500' : ''}} hover:text-white hover:bg-gray-700">
                         Contact Us
                     </a>
 
@@ -78,7 +78,7 @@
                             Dashboard
                         </a>
                     @else
-                        <a href="{{route('cart')}}" class="flex gap-2 px-3 py-2 my-2 lg:mt-2 transition-colors duration-300 transform rounded-md {{Request::routeIs('cart') ? 'active' : ''}} hover:text-white hover:bg-gray-700">
+                        <a href="{{route('cart')}}" class="flex gap-2 px-3 py-2 my-2 lg:mt-2 transition-colors duration-300 transform rounded-md {{Request::routeIs('cart') ? 'active text-pink-500' : ''}} hover:text-white hover:bg-gray-700">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                             </svg>
