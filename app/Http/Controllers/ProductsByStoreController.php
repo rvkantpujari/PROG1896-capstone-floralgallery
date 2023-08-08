8946203865dca470ccd49b13bbe770cb59fe529b
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-08-08 14:23:21 -0400

    add: products by store controller

---
+++ app/Http/Controllers/ProductsByStoreController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ProductsByStoreController extends Controller
+{
+    function show(Request $request)
+    {
+        $store = '';
+        
+        if($request->route('store') !== null)
+            $store = $request->route('store');
+        
+        $seller = DB::table('sellers')->where('store_name', $store)->first();
+
+        if($seller === null)
+            return redirect()->route('home');
+
+        $categories = DB::table('product_categories')->get();
+
+        $products = DB::table('product_categories')->join('products', 'products.seller_id',"=",'product_categories.id')
+                        ->select('products.id as product_id', 'products.*', 'product_categories.id as seller_id', 'product_categories.category')
+                        ->where('products.product_status', '=', 'published')
+                        ->where('products.seller_id', $seller->id)
+                        ->paginate(6);
+        
+        return view('search', ['categories' => $categories, 'store_name' => $store ,'products' => $products]);
+    }
+}
